bbfeb45d8c6caf2746f82246b7ac03e6429aa1f8
Eduardo Couto Rodrigues <eduardocoouto@gmail.com>
2019-05-03 09:51:16 -0300

    Tela de estabelecimento

--- src/app/estabelecimento/estabelecimento.page.ts
+++ src/app/estabelecimento/estabelecimento.page.ts
@@ -7,15 +7,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./estabelecimento.page.scss'],
 })
 export class EstabelecimentoPage implements OnInit {
-  public produtos: Array<{ id:number;  nome: string; descricao: string; imageURL: string }> = [];
+  public produtos: Array<{ id:number;  nome: string; descricao: string; preco: string; imageURL: string }> = [];
   private nomeEstabelecimento: String;
 
   constructor(private route: ActivatedRoute) {
     for (let i = 1; i < 21; i++) {
       this.produtos.push({
         id: i,
-        nome: 'Produto ' + i,
-        descricao: 'Descricao',
+        nome: 'Wiki Mac ' + i,
+        descricao: 'Pão, hamburguer, Alface, Ketchup, Frango e Tomate',
+        preco : '12,00',
         imageURL: "https://pm1.narvii.com/6397/3f28d6c6977ee9268e7534da29cd54fce702d929_128.jpg"
       });
     }
