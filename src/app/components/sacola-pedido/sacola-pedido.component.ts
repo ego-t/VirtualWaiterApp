355fc9b2448c08c2ebbb12fd95b141c19a84bfa9
Eduardo Couto Rodrigues <eduardocoouto@gmail.com>
2019-05-21 00:49:31 -0300

    - Componente Sacola Pedido

--- src/app/components/sacola-pedido/sacola-pedido.component.ts
+++ src/app/components/sacola-pedido/sacola-pedido.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SacolaPedidoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  abrirPaginaPedido(){
+    this.router.navigate(['/pedido'])
+  }
 
   ngOnInit() {}
 
